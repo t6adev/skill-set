0862e41edab7154b71402912160b813ad08f7e1c
tell-y <t.yamamoto28@gmail.com>
2018-03-07 19:45:02 +0900

    add router as simple

--- react/app/src/client/components/App.jsx
+++ react/app/src/client/components/App.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import Counter from './Counter';
 
+const Main = () => <div>Hello World</div>;
+
 export default () => (
-  <div>
-    <div>Hello World</div>
-    <Counter />
-  </div>
+  <Router>
+    <div>
+      <ul>
+        <li>
+          <Link to="/">main</Link>
+        </li>
+        <li>
+          <Link to="/counter">counter</Link>
+        </li>
+      </ul>
+      <Route exact path="/" component={Main} />
+      <Route path="/counter" component={Counter} />
+    </div>
+  </Router>
 );
